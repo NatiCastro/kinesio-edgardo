428407e04f4868cf39fd2733ac25263e0818bbf3
Natalia Castro <castronatib@gmail.com>
2022-07-28 17:24:16 -0300

    ultimos cambios

--- script.js
+++ script.js
@@ -1,50 +1,15 @@
 
 function homeView(padre) {
     $(padre).html(`
-        <div class="seccion-mobile">
-        <h1>Kinesiología Deportiva</h1>
-        <div class="imagen-principal">
-          <img src="./Images/principal1.jpg" alt="maniobra-kinesiologia">
-          </div>
+        <div class="container-desk">
+        <img class="imagen-desk" src="./Images/carousel-1.jpg" alt="maniobra">
         </div>
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          <div class="seccion-mobile">
+          <h1>Kinesiología Deportiva</h1>
+          <div class="imagen-principal">
+            <img src="./Images/principal1.jpg" alt="maniobra-kinesiologia">
+            </div>
         </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active" data-bs-interval="3000">
-            <img src="./Images/carousel-1.jpg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5 class="titulo-carousel">Kinesiología Deportiva</h5>
-              <p class="parrafo-carousel">Zona Oeste | Morón | Castelar | Padua</p>
-            </div>
-          </div>
-          <div class="carousel-item" data-bs-interval="3000">
-            <img src="./Images/carousel-2.jpg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5 class="titulo-carousel">Kinesiología Deportiva</h5>
-              <p class="parrafo-carousel">Zona Oeste | Morón | Castelar | Padua</p>
-            </div>
-          </div>
-          <div class="carousel-item" data-bs-interval="3000">
-            <img src="./Images/carousel-3.jpg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5 class="titulo-carousel">Kinesiología Deportiva</h5>
-              <p class="parrafo-carousel">Zona Oeste | Morón | Castelar | Padua</p>
-            </div>
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
         <h2 class="nombre">Edgardo Castro</h2>
         <h5 class="profesion">Kinesiologo especialista en Kinesiología Deportiva</h5>
         <h6 class="universidad">Universidad de Morón <br> Universidad de Favaloro</h6>
@@ -79,6 +44,7 @@ function homeView(padre) {
     `);
 }
 
+
 function servicesView(padre) {
     $(padre).html(`
         <section class="servicios">
@@ -103,22 +69,12 @@ function servicesView(padre) {
 function contactView(padre) {
     $(padre).html(`
         <section class="seccion-contacto">
-            <h1>Contacto</h1>
-            <form id="enviar" class="form" action="">
-            <label class="form_label" for="name">Nombre</label>
-            <input class="form_input" type="text" required="required" placeholder="Nombre"/>
-            <label class="form_label" for="email">Email</label>
-            <input id="email" 
-            class="form_input" 
-            type="email" 
-            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-            placeholder="Email"
-            />
-            <h5 id="result"></h5>
-            <label class="form_label" for="text">Mensaje</label>
-            <textarea name="message" id="" cols="30" rows="9" required="required"></textarea>
-            <input class="input_enviar" type="submit" value="Enviar" />
-          </form>
+            <h1>Encontrame en</h1> 
+            <div class="container-contacto">
+            <span class="span-contacto">Kinesia Castelar: Arias 1234, Castelar</span>          
+            <span class="span-contacto">Kinesia Haedo: Calle falsa 1234, Haedo</span>              
+            <span class="span-contacto">C.D. Argentino de Merlo: Calle falsa 1234, Merlo</span>              
+            </div>     
           <div class="footer fixed-bottom">
           <span class="parrafo-footer">Copyright 2022 | Creado por NatDev</span>
           <a href="#"><i class="bi bi-instagram icono-insta"></i></a>
@@ -161,7 +117,7 @@ const router = () => {
             break;
         
         case "servicios":
-            servicesView('#app');
+            servicesView('#app');           
             break;
         
         case "contacto":
@@ -176,7 +132,7 @@ const router = () => {
 
 // Llamo a la función router
 
-$(window).on("load", function() {
+$(window).on('load', function() {
     router();
 })
 
@@ -185,46 +141,4 @@ $(window).on("hashchange", function() {
 })
 
 
-// Formulario
 
-
-let inputEmail = $("#email").val();
-console.log(inputEmail);
-
-const validateEmail = (email) => {
-  return email.match(
-    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  );
-};
-
-const validate = () => {
-  let resultado = $("#result");
-  let email = $("#email").val();
-  resultado.text ('');
-
-  if (validateEmail(email)) {
-    console.log(email);
-    resultado.text(email + ' es válido');
-    resultado.css('color', 'green');
-  } else {
-    resultado.text(email + ' es inválido');
-    resultado.css('color', 'red');
-  }
-  return false;
-}
-
-$('#email').on("input", validate);
-
-$(()=>{
-
-
-$('#enviar').on("submit", function(e) {
-    e.preventDefault();
-
-    console.log("Mensaje enviado");
-
-    e.submit;
-
-})
-
-})
